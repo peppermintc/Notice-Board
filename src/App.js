58014852179e7948bd906c384afc8a5ba7a0df68
peppermintc <haminjp@naver.comm>
2020-05-26 07:49:44 +0900

    CRUD

--- src/App.js
+++ src/App.js
@@ -1,31 +1,88 @@
 import React, { useState } from 'react';
 
+let dummy = ['This is example Notice 1', 'This is example Notice 2'];
+
 function App() {
+  // DOM
+  const noticeEditTextInputBox = document.getElementById("noticeEditTextInputBox");
+  const createTool = document.getElementById("createTool");
+  const editTool = document.getElementById("editTool");
+  const functionButtons = Array.prototype.slice.call(document.getElementsByName("functionButtons"));
+  
+  // Hook
   const [notice, setNotice] = useState('');
-  const [noticeList, setNoticeList] = useState([]);
+  const [noticeList, setNoticeList] = useState(dummy);
+  const [selectedNoticeIndex, setSelectedNoticeIndex] = useState();
+  const [editMode, setEditMode] = useState(false);
   
   const onChangeNoticeInput = (e) => {
+    setNotice(e.target.value);
+  };
+  const onChangeNoticeEditInput = (e) => {
     setNotice(e.target.value);
   };
   const createNotice = () => {
     setNoticeList([...noticeList, notice]);
   };
-  const deleteNotice = (index) => {
-    setNoticeList([...noticeList].filter(notice => [...noticeList].indexOf(notice) !== index));
+  const deleteNotice = (indexToDelete) => {
+    setNoticeList([...noticeList].filter((notice, index) => index !== indexToDelete));
   };
-  const updateNotice = (index) => {
+  const toggleEditTextBox = (index) => {
+    // Toggle Mode
+    if (editMode === true) {
+      setEditMode(false);
+      createTool.style.display = 'block';
+      editTool.style.display = 'none';
+      functionButtons.map((buttons) => buttons.style.display = 'block');
+    }
+    else {
+      setEditMode(true);
+      createTool.style.display = 'none';
+      editTool.style.display = 'block';
+      functionButtons.map((buttons) => buttons.style.display = 'none');
+    }
+
+    setSelectedNoticeIndex(index);
+  };
+  const updateNotice = () => {
     let newList = [...noticeList];
-    newList[index] = "Changed"     
+    newList[selectedNoticeIndex] = noticeEditTextInputBox.value;     
     setNoticeList(newList);
+    toggleEditTextBox();
   };
 
+  // HTML
+  const noticeHTML = (notice, index) => {
+    return (
+      <div>
+        Index: {index}<br/>
+        Notice: {notice}
+        <div name="functionButtons" style={{ display: 'block' }}>
+          <button onClick={() => toggleEditTextBox(index)}>Edit</button>
+          <button onClick={() => deleteNotice(index)}>Delete</button>
+        </div>
+      </div> 
+    );
+  };
+
+  // JSX
   return (
     <div>
       <div>Notice List Length - {noticeList.length}</div>
-      {noticeList.map((notice, index) => { return <div key={index}>{notice}<button onClick={() => updateNotice(index)}>Edit</button><button onClick={() => deleteNotice(index)}>Delete</button></div> })}
-      <div>Shows Notice - {notice}</div>
-      <input type="text" onChange={onChangeNoticeInput} />
-      <button onClick={createNotice}>Save Notice</button>
+      {noticeList.map((notice, index) => { return noticeHTML(notice, index); })}
+      <div>Shows Current Writting Notice: {notice}</div>
+      
+      <div style={{display: 'block'}} id="createTool">
+        <div>Create Notice</div>
+        <input type="text" onChange={onChangeNoticeInput} />
+        <button onClick={createNotice}>Save Notice</button>
+      </div>
+      
+      <div style={{display: 'none'}} id="editTool">
+        <div>Edit Notice index: {selectedNoticeIndex}</div>
+        <input type="text" id="noticeEditTextInputBox" onChange={onChangeNoticeEditInput} />
+        <button onClick={updateNotice}>Save Notice</button>
+      </div>
     </div>
   );
 }
