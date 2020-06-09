cbdb20ba53a7ec5eeed0766e8df0964d8d09240c
peppermintc <haminjp@naver.com>
2020-06-09 16:43:03 +0900

    Updates

---
+++ precache-manifest.71f701efd73520a071d38b5fe2da6ae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be95fad60c06cb1202ce8b73c6ca3e8",
+    "url": "/Notice-Board/index.html"
+  },
+  {
+    "revision": "8f734b7b03e8f709b3b5",
+    "url": "/Notice-Board/static/css/main.97708c48.chunk.css"
+  },
+  {
+    "revision": "dc136fecfa7e054099af",
+    "url": "/Notice-Board/static/js/2.80ed73d3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Notice-Board/static/js/2.80ed73d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f734b7b03e8f709b3b5",
+    "url": "/Notice-Board/static/js/main.ab3f04b0.chunk.js"
+  },
+  {
+    "revision": "a4e7de9bc66959bf8fe3",
+    "url": "/Notice-Board/static/js/runtime-main.43010a0e.js"
+  },
+  {
+    "revision": "7171d6b224b14ff8ccf891de18bf696d",
+    "url": "/Notice-Board/static/media/logo.7171d6b2.png"
+  }
+]);
